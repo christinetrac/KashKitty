45e2e207e95d6eb2eaa87188aaa246f9593fd9e6
Camila B <Lcamila.bustos@yahoo.com>
2021-01-08 22:39:59 -0600

    Added icons to tabs

--- Components/TabNavigator.js
+++ Components/TabNavigator.js
@@ -1,22 +1,39 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Home } from '../Screens/Home';
-import { Necessities } from '../Screens/Necessities';
-import { Personal } from '../Screens/Personal';
-import { Entertainment } from '../Screens/Entertainment';
+import { Home } from "../Screens/Home";
+import { Necessities } from "../Screens/Necessities";
+import { Personal } from "../Screens/Personal";
+import { Entertainment } from "../Screens/Entertainment";
+import { FontAwesome } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
-    return (
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Necessities" component={Necessities} />
-        <Tab.Screen name="Personal" component={Personal} />
-        <Tab.Screen name="Entertainment" component={Entertainment} />
-      </Tab.Navigator>
-    );
-  };
-  
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === "Home") {
+            return <FontAwesome name="paw" size={size} color={color} />;
+          } else if (route.name === "Necessities") {
+            return (
+              <FontAwesome5 name="toilet-paper" size={size} color={color} />
+            );
+          } else if (route.name === "Personal") {
+            return <FontAwesome name="heart" size={size} color={color} />;
+          } else if (route.name === "Entertainment") {
+            return <FontAwesome5 name="utensils" size={size} color={color} />;
+          }
+        },
+      })}
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Necessities" component={Necessities} />
+      <Tab.Screen name="Personal" component={Personal} />
+      <Tab.Screen name="Entertainment" component={Entertainment} />
+    </Tab.Navigator>
+  );
+};
+
 export { BottomTabNavigator };
-  
