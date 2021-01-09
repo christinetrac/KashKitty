711a6c30f947016df553ec19397153a6dc6c8efc
christinetrac <christineetrac@gmail.com>
2021-01-09 03:34:28 -0500

    resolved all merge conflicts and app issues

--- Components/TransactionPage.js
+++ Components/TransactionPage.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export const TransactionPage = (props) => {
+    const [text, setText] = useState('');
     return (
         <View style={styles.container}>
-            <Text>
-                Transaction Page
-            </Text>
+            <TextInput
+                value={text}
+                placeholder="Input Spending Amount..."
+                onChangeText={(text) => value.match(/[0-9]*/gm) && setText(text)}
+            />
         </View>
     )
 };
