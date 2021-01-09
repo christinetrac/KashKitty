e90464a45d903d78fa4fb6a59141cd3f91866ac2
Camila B <Lcamila.bustos@yahoo.com>
2021-01-09 00:42:12 -0600

    Created budget bar thingy

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
+import BudgetBar from "../Components/BudgetBar";
+
+// const red = Math.min(2 - (2 * Fraction), 1) * 255; const green = Math.min(2 * Fraction, 1) * 255;
 export const Home = (props) => {
-    return (
-        <View style={styles.container}>
-            <Text>
-                Home
-            </Text>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <BudgetBar style={{ alignSelf: "flex-start" }} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+});
