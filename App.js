2fc656c49e2cb349cb0d5fc4bd8cf35dc1a4d7aa
christinetrac <christineetrac@gmail.com>
2021-01-09 00:35:07 -0500

    Added transaction page and constants

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
 import { BottomTabNavigator } from './Components/TabNavigator';
-import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
-      <NavigationContainer style={styles.navigationContainer}>
-          <BottomTabNavigator/>
-        </NavigationContainer>
+      <BottomTabNavigator/>
     </SafeAreaView>
   );
 }
@@ -20,6 +17,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  navigationContainer: {
-  }
 });
