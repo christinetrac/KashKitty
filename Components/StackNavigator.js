fb00045e2907e42998327a6d5fa82ea92ff908d4
Camila B <Lcamila.bustos@yahoo.com>
2021-01-09 12:08:45 -0600

    Now shows new transaction page for each tab

--- Components/StackNavigator.js
+++ Components/StackNavigator.js
@@ -1,48 +1,76 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Home } from '../Screens/Home';
-import { TransactionPage } from './TransactionPage';
-import { Necessities } from '../Screens/Necessities';
-import { Personal } from '../Screens/Personal';
-import { Entertainment } from '../Screens/Entertainment';
+import { Home } from "../Screens/Home";
+import { TransactionPage } from "./TransactionPage";
+import { Necessities } from "../Screens/Necessities";
+import { Personal } from "../Screens/Personal";
+import { Entertainment } from "../Screens/Entertainment";
 
 const Stack = createStackNavigator();
 
 const screenOptionStyle = {
-    headerShown: false
+  headerShown: false,
 };
 
 const StackNavigator = () => {
-    return (
-      <Stack.Navigator>
-        <Stack.Screen name="Home" options={{headerShown: false}} component={Home} />
-        <Stack.Screen name="TransactionPage" options={{headerShown: true}} component={TransactionPage} />
-      </Stack.Navigator>
-    );
-}
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        options={{ headerShown: false }}
+        component={Home}
+      />
+      <Stack.Screen
+        name="TransactionPage"
+        options={{ headerShown: true }}
+        component={TransactionPage}
+      />
+    </Stack.Navigator>
+  );
+};
 
 const NecessitiesStackNavigator = () => {
-    return (
-        <Stack.Navigator screenOptions={screenOptionStyle}>
-          <Stack.Screen name="Necessities" component={Necessities} />
-        </Stack.Navigator>
-    );
-}
+  return (
+    <Stack.Navigator screenOptions={screenOptionStyle}>
+      <Stack.Screen name="Necessities" component={Necessities} />
+      <Stack.Screen
+        name="TransactionPage"
+        options={{ headerShown: true }}
+        component={TransactionPage}
+      />
+    </Stack.Navigator>
+  );
+};
 
 const PersonalStackNavigator = () => {
-    return (
-        <Stack.Navigator screenOptions={screenOptionStyle}>
-          <Stack.Screen name="Personal" component={Personal} />
-        </Stack.Navigator>
-    );
-}
+  return (
+    <Stack.Navigator screenOptions={screenOptionStyle}>
+      <Stack.Screen name="Personal" component={Personal} />
+      <Stack.Screen
+        name="TransactionPage"
+        options={{ headerShown: true }}
+        component={TransactionPage}
+      />
+    </Stack.Navigator>
+  );
+};
 
 const EntertainmentStackNavigator = () => {
-    return (
-        <Stack.Navigator screenOptions={screenOptionStyle}>
-          <Stack.Screen name="Entertainment" component={Entertainment} />
-        </Stack.Navigator>
-    );
-}
+  return (
+    <Stack.Navigator screenOptions={screenOptionStyle}>
+      <Stack.Screen name="Entertainment" component={Entertainment} />
+      <Stack.Screen
+        name="TransactionPage"
+        options={{ headerShown: true }}
+        component={TransactionPage}
+      />
+    </Stack.Navigator>
+  );
+};
 
-export { StackNavigator, NecessitiesStackNavigator, PersonalStackNavigator, EntertainmentStackNavigator };
+export {
+  StackNavigator,
+  NecessitiesStackNavigator,
+  PersonalStackNavigator,
+  EntertainmentStackNavigator,
+};
